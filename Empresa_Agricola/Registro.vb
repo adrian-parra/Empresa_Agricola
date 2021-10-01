a35605aa6dbb6d53c40856eadf16734e62009b2b
adrian <adrian.adrianborquez@gmail.com>
2021-10-01 15:08:18 -0600

    compilacion

--- Empresa_Agricola/Registro.vb
+++ Empresa_Agricola/Registro.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.Sql
 Imports System.Data.SqlClient
 Public Class Registro
+    Dim cn As New SqlConnection
     Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles txt_newpass.TextChanged
 
     End Sub
@@ -8,4 +9,24 @@ Public Class Registro
     Private Sub Registro_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btn_registrar_Click(sender As Object, e As EventArgs) Handles btn_registrar.Click
+        cn.ConnectionString = conexionBD.conexion
+        Dim adaptador As New SqlCommand("insert into Usuario values ('" + txt_nombreCom.Text + "','" + txt_usuario.Text + "' ,'" + txt_pass.Text + "','" + txt_newpass.Text + "')", cn)
+
+        Try
+            cn.Open()
+            MsgBox("Conexion exitosa")
+            adaptador.ExecuteNonQuery()
+            MsgBox("insert ")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            cn.Close()
+
+        End Try
+    End Sub
+
+
 End Class
--- Empresa_Agricola/conexionBD.vb
+++ Empresa_Agricola/conexionBD.vb
@@ -1,3 +1,5 @@
-﻿Module conexionBD
+﻿
+Module conexionBD
+    Public conexion As String = "Data Source=DESKTOP-686JJVQ;Initial Catalog=cale;Integrated Security=True"
 
 End Module
