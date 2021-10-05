271e2ac8d380247378515ea04923536d1eda75e8
adrian <adrian.adrianborquez@gmail.com>
2021-10-05 17:13:21 -0600

    prueba en paralelo

--- Empresa_Agricola/M_ConexionBD.vb
+++ Empresa_Agricola/M_ConexionBD.vb
@@ -3,4 +3,6 @@ Imports System.Data.SqlClient
 Module M_ConexionBD
     Public conexion As String = "Data Source=DESKTOP-686JJVQ;Initial Catalog=DB_Empresa_Agricola;Integrated Security=True"
     Public cn As New SqlConnection
+
+    Public pru As String
 End Module
