200f33daf16cdf421a61ee91c1e31cf8b35f345f
adrian <adrian.adrianborquez@gmail.com>
2021-10-05 18:22:55 -0600

    conflicto em modulo M_Conexion

--- Empresa_Agricola/M_ConexionBD.vb
+++ Empresa_Agricola/M_ConexionBD.vb
@@ -4,10 +4,5 @@ Module M_ConexionBD
     Public conexion As String = "Data Source=DESKTOP-686JJVQ;Initial Catalog=DB_Empresa_Agricola;Integrated Security=True"
     Public cn As New SqlConnection
 
-<<<<<<< HEAD
-    Public pru As String
-=======
-    Public prueba As Integer
 
->>>>>>> e903247a66c0f1b55be4f3b1665ec0c437398745
 End Module
