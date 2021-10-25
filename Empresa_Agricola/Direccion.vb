a90db31141e3dfeb33df1194a16bca99b6be76d2
adrian <adrian.adrianborquez@gmail.com>
2021-10-24 23:26:57 -0600

    agregar nuevo cliente ,direccion

--- Empresa_Agricola/Direccion.vb
+++ Empresa_Agricola/Direccion.vb
@@ -31,24 +31,83 @@ Public Class Direccion
 
     End Sub
 
+    'proceso asincrona
+    Sub Registrar()
+        If M_Direccion.TipoDireccion = "cliente" Then
+            VentaNewCliente.RegistrarCliente()
+        End If
+    End Sub
+
     Private Sub BTN_Registrar_Direccion_Click(sender As Object, e As EventArgs) Handles BTN_Registrar_Direccion.Click
         If TXT_Num_Exterior.Text = "" Then
-
+            MsgBox("todos lo campos de texto son requeridos")
         ElseIf TXT_Colonia.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         ElseIf TXT_Codigo_Postal.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         ElseIf TXT_Calle.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         ElseIf CB_Ciudad.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         ElseIf CB_Municipios.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         ElseIf CB_Estados.Text = "" Then
+            MsgBox("todos lo campos de texto son requeridos")
 
         Else
             'CAMPOS VALIDADOS
 
+            'OBTENER ID DE ESTADO,MUNICIPIO Y LOCALIDAD
+            Dim idest As String = idestados(CB_Estados.SelectedIndex)
+            Dim idmun As String = idmunicipios(CB_Municipios.SelectedIndex)
+            Dim idloc As String = idlocalidades(CB_Ciudad.SelectedIndex)
+
+
+            'INSERTAR REGISTRO EN TABLA DIRECCIONES
+
+
+            Try
+                'ABRIR CONEXION BD
+                cn.Open()
+
+                'CONSULTA INSERT
+                Dim query_insert_direcccion As New SqlCommand("insert into Direcciones values ('" + TXT_Num_Exterior.Text + "', '" + TXT_Calle.Text + "','" + TXT_Colonia.Text + "','" + idloc + "','" + idmun + "','" + idest + "','" + TXT_Codigo_Postal.Text + "','" + M_Direccion.TipoDireccion + "')", cn)
+
+                'EJECUTAR CONSULTA
+                query_insert_direcccion.ExecuteNonQuery()
+
+                'OBTENER ID DE REGISTRO
+                Dim query_id_direccion As New SqlCommand("select Max(ID_direccion) from Direcciones", cn)
+                Dim leerdatos As SqlDataReader = query_id_direccion.ExecuteReader
+
+                leerdatos.Read()
+
+                M_Direccion.IDdireccion = leerdatos.GetValue(0)
+
+                MsgBox("Direccion registrada")
+
+                'BLOQUEAR BTN DIRECCION
+                'VentaNewCliente.BTN_Agregar_Direccion.Enabled = False
+
+                'CERRAR CONEXION BD
+                cn.Close()
+                leerdatos.Close()
+
+                'ejecutar proceso asyncrono
+                Registrar()
+
+                'CERRAR LA VENTANA
+                Me.Close()
+
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
 
         End If
     End Sub
@@ -96,7 +155,7 @@ Public Class Direccion
         Dim leerdatos As SqlDataReader = query_localidades.ExecuteReader
 
         While leerdatos.Read
-            idmunicipios.Add(leerdatos.GetValue(0))
+            idlocalidades.Add(leerdatos.GetValue(0))
             CB_Ciudad.Items.Add(leerdatos.GetValue(3))
 
         End While
--- Empresa_Agricola/VentaNewCliente.vb
+++ Empresa_Agricola/VentaNewCliente.vb
@@ -1,7 +1,7 @@
-﻿Public Class VentaNewCliente
+﻿Imports System.Data.SqlClient
+Public Class VentaNewCliente
 
-
-    Private Sub BTN_Registrar_Cliente_Click(sender As Object, e As EventArgs) Handles BTN_Registrar_Cliente.Click
+    Sub RegistrarCliente()
         'VALIDAR TXT 
         If TXT_Correo.Text = "" Then
             MsgBox("campos de textos obligatorios")
@@ -18,18 +18,50 @@
             Else
                 'TODO VALIDADDO
 
+                Try
+                    'ABRIR CONEXION BD
+                    cn.Open()
+
+                    'CONSULTA ADD CLIENTE
+                    Dim query_add_cliente As New SqlCommand("insert into Cliente values('" + M_Direccion.IDdireccion + "','" + TXT_Nombre.Text + "','" + TXT_Telefono.Text + "','" + TXT_Correo.Text + "','" + TXT_Rfc.Text + "')", cn)
+                    query_add_cliente.ExecuteNonQuery()
+
+                    MsgBox("cliente agregado")
+                    M_Direccion.IDdireccion = ""
+                    M_Direccion.TipoDireccion = ""
+
+                    BTN_Agregar_Direccion.Enabled = True
+
+                    cn.Close()
+
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+
             End If
         End If
     End Sub
 
-    Private Sub VentaNewCliente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub BTN_Registrar_Cliente_Click(sender As Object, e As EventArgs) Handles BTN_Registrar_Cliente.Click
+
+
+        RegistrarCliente()
+
+    End Sub
+
+    Private Sub VentaNewCliente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'PROPIEDADES DE CONEXION BD
+        cn.ConnectionString = conexion
     End Sub
 
     Private Sub BTN_Agregar_Direccion_Click(sender As Object, e As EventArgs) Handles BTN_Agregar_Direccion.Click
         'AGREGAR AL MODULO TIPO DE DIRECCION
-        M_Direccion.TipoDireccion = "Cliente"
+        M_Direccion.TipoDireccion = "cliente"
         Direccion.Show()
+
+        BTN_Agregar_Direccion.Enabled = False
+
 
     End Sub
 End Class
